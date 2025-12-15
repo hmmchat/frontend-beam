4cbfd714f5d1ce97055474ae005076dcb8ba271b
Kshitiz Maurya <kshitizmaurya@Kshitizs-MacBook-Pro.local>
2025-12-15 18:01:46 +0530

    d

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,9 @@ import "./globals.css";
 export const metadata = {
   title: "Hmm..",
   description: "Meet Someone Here , Not Sure Who , But Someone",
+  icons: {
+    icon: "/assets/Logo.svg",
+  },
 };
 
 export default function RootLayout({ children }) {
